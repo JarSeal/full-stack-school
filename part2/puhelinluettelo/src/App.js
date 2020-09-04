acfa6b53360b2b53b291bd937ee263247c7f5e07
JarSeal <kai.forsman@gmail.com>
2020-09-04 18:37:13 +0300

    Refactoring.

--- part2/puhelinluettelo/src/App.js
+++ part2/puhelinluettelo/src/App.js
@@ -14,7 +14,7 @@ const App = () => {
     axios
       .get('http://localhost:3001/persons')
       .then(response => {
-        setPersons(response.data)
+        setPersons(response.data);
       })
   }, []);
 
