816b718c666895db8c0482f3a159dd98ec76f70b
JarSeal <kai.forsman@gmail.com>
2020-10-21 10:56:24 +0300

    Part 4: blogilista 4.2 done.

--- part4/blogilista/index.js
+++ part4/blogilista/index.js
@@ -1,57 +1,10 @@
-require('dotenv').config();
+const app = require('./app');
 const http = require('http');
-const express = require('express');
-const app = express();
-const cors = require('cors');
-const mongoose = require('mongoose');
+const config = require('./utils/config');
+const logger = require('./utils/logger');
 
-app.use(express.static('build'));
-app.use(cors());
-app.use(express.json());
+const server = http.createServer(app);
 
-const Blog = require('./models/blog');
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs);
-    })
-    .catch(error => next(error));
-});
-
-app.post('/api/blogs', (request, response) => {
-    const blog = new Blog(request.body);
-  
-    blog
-      .save()
-      .then(result => {
-        response.status(201).json(result);
-    })
-    .catch(error => next(error));
-});
-
-const unknownEndpoint = (req, res) => {
-  res.status(404).send({ error: 'unknown endpoint' });
-};
-app.use(unknownEndpoint);
-
-const errorHandler = (error, req, res, next) => {
-  console.error(error.message);
-
-  // if (error.name === 'CastError') {
-  //   return res.status(400).send({ error: 'malformatted id' });
-  // } else if (error.name === 'ValidationError') {
-  //   return res.status(400).send({ error: error.message });
-  // }
-
-  return res.status(400).send({ error: 'unspecified error' });
-
-  next(error);
-};
-app.use(errorHandler);
-
-const PORT = process.env.PORT;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`);
 });
