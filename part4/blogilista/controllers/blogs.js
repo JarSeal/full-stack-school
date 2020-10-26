429034bd5d4b5245b913267fa8881c69e981a47a
JarSeal <kai.forsman@gmail.com>
2020-10-26 04:57:32 +0200

    Part 4: blogilista, 4.13 ja 4.14.

--- part4/blogilista/controllers/blogs.js
+++ part4/blogilista/controllers/blogs.js
@@ -13,4 +13,23 @@ blogsRouter.post('/', async (request, response) => {
   response.json(savedBlog.toJSON());
 });
 
+blogsRouter.delete('/:id', async (req, res, next) => {
+  const result = await Blog.findByIdAndRemove(req.params.id);
+  if(result) {
+    res.status(204).end();
+  } else {
+    res.status(404).end();
+  }
+});
+
+blogsRouter.put('/:id', async (req, res, next) => {
+  const body = req.body;
+  const result = await Blog.findByIdAndUpdate(req.params.id, body, { new: true });
+  if(result) {
+    res.json(result);
+  } else {
+    res.status(404).end();
+  }
+});
+
 module.exports = blogsRouter;
--- part4/blogilista/tests/blog_api.test.js
+++ part4/blogilista/tests/blog_api.test.js
@@ -54,51 +54,85 @@ beforeEach(async () => {
 });
 
 describe('api tests', () => {
-  test('there are '+initialBlogs.length+' blogs with id attribute', async () => {
+  test('there are '+initialBlogs.length+' initial blogs with id\'s', async () => {
     const response = await api.get('/api/blogs');
     expect(response.body).toHaveLength(initialBlogs.length);
     for(let blog of response.body) {
       expect(blog.id).toBeDefined();
     }
   });
-  test('new blog is added succesfully', async () => {
-    const blog = new Blog({
-      title: 'Lets test this blog',
-      author: 'Bot',
-      url: 'http://www.yle.fi',
-      likes: 0
+  describe('add a blog', () => {
+    test('new blog is added succesfully', async () => {
+      const blog = new Blog({
+        title: 'Lets test this blog',
+        author: 'Bot',
+        url: 'http://www.yle.fi',
+        likes: 0
+      });
+      await blog.save();
+      const response = await api.get('/api/blogs');
+      expect(response.body).toHaveLength(initialBlogs.length + 1);
     });
-    await blog.save();
-    const response = await api.get('/api/blogs');
-    expect(response.body).toHaveLength(initialBlogs.length + 1);
+    test('new blog without likes is added succesfully and the number of likes is defaulted to zero', async () => {
+      const blog = new Blog({
+        title: 'Lets test a blog without defining likes',
+        author: 'Bot',
+        url: 'http://www.yle.fi'
+      });
+      let savedBlog = await blog.save();
+      savedBlog = savedBlog.toJSON();
+      expect(savedBlog.likes).toBe(0);
+    });
+    test('new blog without title fails as bad request (validationError)', async () => {
+      const blog = new Blog({
+        author: 'Bot',
+        url: 'http://www.yle.fi'
+      });
+      await expect(blog.save())
+        .rejects
+        .toThrow(mongoose.Error.ValidationError);
+    });
+    test('new blog without url fails as bad request (validationError)', async () => {
+      const blog = new Blog({
+        title: 'Test blog',
+        author: 'Bot'
+      });
+      await expect(blog.save())
+        .rejects
+        .toThrow(mongoose.Error.ValidationError);
+    });
   });
-  test('new blog without likes is added succesfully', async () => {
-    const blog = new Blog({
-      title: 'Lets test a blog without defining likes',
-      author: 'Bot',
-      url: 'http://www.yle.fi'
+  describe('delete a blog', () => {
+    test('delete a blog successfully', async () => {
+      const blogs = await api.get('/api/blogs');
+      const delId = blogs.body[0].id;
+      const result = await Blog.findByIdAndRemove(delId);
+      const blogsAfterDel = await api.get('/api/blogs');
+      expect(result.id).toBe(delId);
+      expect(blogsAfterDel.body.length).toBe(blogs.body.length - 1);
     });
-    let savedBlog = await blog.save();
-    savedBlog = savedBlog.toJSON();
-    expect(savedBlog.likes).toEqual(0);
+    test('try to delete a blog with a non existing id', async () => {
+      const blogs = await api.get('/api/blogs');
+      const nonExistingId = mongoose.Types.ObjectId();
+      const result = await Blog.findByIdAndRemove(nonExistingId);
+      const blogsAfterDelAttempt = await api.get('/api/blogs');
+      expect(result).toBe(null);
+      expect(blogsAfterDelAttempt.body.length).toBe(blogs.body.length);
+    });
   });
-  test('new blog without title fails as bad request (validationError)', async () => {
-    const blog = new Blog({
-      author: 'Bot',
-      url: 'http://www.yle.fi'
+  describe('edit a blog', () => {
+    test('edit a blog successfully by adding likes', async () => {
+      const blogs = await api.get('/api/blogs');
+      const newLikes = blogs.body[0].likes + 1;
+      const id = blogs.body[0].id;
+      const result = await Blog.findByIdAndUpdate(id, { likes: newLikes }, { new: true });
+      expect(result.likes).toBe(newLikes);
     });
-    await expect(blog.save())
-      .rejects
-      .toThrow(mongoose.Error.ValidationError);
-  });
-  test('new blog without url fails as bad request (validationError)', async () => {
-    const blog = new Blog({
-      title: 'Test blog',
-      author: 'Bot'
+    test('try to edit a non existing blog', async () => {
+      const nonExistingId = mongoose.Types.ObjectId();
+      const result = await Blog.findByIdAndUpdate(nonExistingId, { likes: 0 }, { new: true });
+      expect(result).toBe(null);
     });
-    await expect(blog.save())
-      .rejects
-      .toThrow(mongoose.Error.ValidationError);
   });
 });
 
