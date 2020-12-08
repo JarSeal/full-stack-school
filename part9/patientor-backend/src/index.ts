69161cd512a32e406c5ece1718930ed0ae39a941
JarSeal <kai.forsman@gmail.com>
2020-12-08 13:25:58 +0200

    Part 9: patientor data, routes and services. 9.10-9.11.

--- part9/patientor-backend/src/index.ts
+++ part9/patientor-backend/src/index.ts
@@ -1,6 +1,10 @@
 import express from 'express';
+import cors = require('cors');
+import patientRouter from './routes/patients';
+import diagnoseRouter from './routes/diagnoses';
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const PORT = 3001;
 
@@ -8,6 +12,9 @@ app.get('/api/ping', (_req, res) => {
     console.log('someone pinged here');
     res.send('pong');
 });
+
+app.use('/api/patients', patientRouter);
+app.use('/api/diagnoses', diagnoseRouter);
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
---
+++ part9/patientor-backend/src/services/patientService.ts
@@ -0,0 +1,23 @@
+import patientData from '../../data/patients.json';
+import { PatientEntry } from '../types';
+
+const patients: Array<PatientEntry> = patientData;
+
+const getEntries = (): Omit<PatientEntry, 'ssn'>[] => {
+    return patients.map(({ id, name, dateOfBirth, gender, occupation }) => ({
+        id,
+        name,
+        dateOfBirth,
+        gender,
+        occupation,
+    }));
+};
+
+const addEntry = (): null => {
+    return null;
+};
+
+export default {
+    getEntries,
+    addEntry
+};
